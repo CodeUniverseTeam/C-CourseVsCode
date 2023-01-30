129c73f73945a5d54fcf2279117f7401318c515c
CodeUniverseTeam <123945322+CodeUniverseTeam@users.noreply.github.com>
2023-01-30 11:59:59 +0100

    Commit for Lesson 3
    In this lesson, we talked about variables and data types as well as arithmetic operations

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//الدرس 3
+//المتغيرات
+// الإعلان عن المتغيرات مثال: السلاسل النصية
+string language = "arabic";
+//var language = "arabic";
+Console.WriteLine(language);
+string name;
+name = "programming for blind";
+Console.WriteLine(name);
+//أنواع البيانات الأخرى
+// المحارف
+char letter = 'm';
+Console.WriteLine(letter);
+// الأرقام الصحيحة
+int age = 26;
+Console.WriteLine(age);
+//العمليات الحسابية
+int n1 = 3;
+int n2 = 5;
+// الجمع
+Console.WriteLine(n1+n2);
+// الطرح
+Console.WriteLine(n1-n2);
+// الضرب
+Console.WriteLine(n1*n2);
+// القسمة
+Console.WriteLine(n1/n2);
+// متبقي القسمة
+Console.WriteLine(n1%n2);
+// حساب القوى
+Console.WriteLine(Math.Pow(3,2));
+// الأرقام العشرية
+double d1 = 5.5;
+double d2 = 5.6;
+double r = d1+d2;
+Console.WriteLine(r);
+// البيانات المنطقية
+bool yes = true;
+bool no = false;
+Console.WriteLine(yes);
+System.Console.WriteLine(no);
